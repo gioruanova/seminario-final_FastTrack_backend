0fbe04767dad0bb0290c7db54732099b110a7df6
Jorge Ruanova <jruanova.dev@gmail.com>
2025-07-17 22:16:44 -0300

    test 8

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 require("dotenv").config();
 
+
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./utils/swagger/swaggerConfig");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8888;
 
-app.use((req, res, next) => {
-  console.log(`Request ${req.method} ${req.url}`);
-  next();
+const corsOptions = {
+  origin: [process.env.BACK_TEST_SITE, process.env.BACK_TEST_SITE_2],
+  methods: "GET,PUT,POST,DELETE",
+  allowedHeaders: "Content-Type",
+};
+
+// =====================================================================
+// Middleware
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// =====================================================================
+// Routes
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/public", require("./src/routes/publicRoutes"));
+app.use("/super", require("./src/routes/superRoutes"));
+app.use(require("./src/routes/userRoutes"));
+
+
+
+// =====================================================================
+// 404 fallback
+app.use((req, res) => {
+  res.status(404).send(`
+    <div style="width: 100%;height: 100%;display: flex;justify-content: center;align-items: center;background: #1a1a1a;flex-direction: column;color: white;">
+        <h1 style="margin: 0;text-transform: uppercase;font-size: 55px;">Root</h1>
+        <a href="/api-docs" style="color: #98c3f1;text-transform: uppercase;text-decoration: none;font-size: 21px;">Check doc</a>
+    </div>
+  `);
 });
 
-app.get("/ping", (req, res) => {
-  res.status(200).json({ message: "pong" });
-});
+// =====================================================================
+// Running
+console.log("✅ About to start server...");
 
-app.listen(port, () => {
-  console.log(`✅ Server running on port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server running on http://localhost:${port}`);
 });
